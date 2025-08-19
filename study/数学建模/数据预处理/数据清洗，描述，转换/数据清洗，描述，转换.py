6285d6afca83358efdb65be848d703e5a3eace79
锦上花一盏 <110328934+jimes3@users.noreply.github.com>
2025-08-19 21:53:03 +0800

    崩了2

--- study/数学建模/数据预处理/数据清洗，描述，转换/数据清洗，描述，转换.py
+++ study/数学建模/数据预处理/数据清洗，描述，转换/数据清洗，描述，转换.py
@@ -14,7 +14,7 @@ pd.set_option('display.max_columns', None)
 #显示所有行
 pd.set_option('display.max_rows', None)
 #设置value的显示长度为100，默认为50
-pd.set_option('max_colwidth',100)
+pd.set_option('max_colwidth',10)
 
 
 ##########################     导入数据      #################################
